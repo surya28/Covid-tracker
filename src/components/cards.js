20062df77c94581c0fbbcd9a3a3a8fcb5c5c18a1
Surya <surya@nfnserver.nfndev.com>
2021-12-21 12:59:29 +0530

    added district filter in state card and state details page done

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const StateCard = ({ state }) => {
+const StateCard = (props) => {
+  const [state, setState] = useState();
   const [districts, setDistricts] = useState(null);
   const history = useNavigate();
+
+  useEffect(() => {
+    setState(props.state);
+  }, [props]);
 
   useEffect(() => {
     let disctrictCopy = [],
@@ -12,7 +17,18 @@ const StateCard = ({ state }) => {
       disctrictCopy.push(key);
     }
     setDistricts(disctrictCopy);
-  }, [state]);
+  }, [props]);
+
+  const handleDistrictChange = (e) => {
+    if (e.target.value === "all") {
+      setState(props.state);
+    } else {
+      setState({
+        ...props.state.districts[e.target.value],
+        state: state.state,
+      });
+    }
+  };
 
   return (
     <article className="card">
@@ -25,8 +41,8 @@ const StateCard = ({ state }) => {
           State - {state?.state}
         </h2>
         <span>
-          <select className="select">
-            <option value="all">Select a district</option>
+          <select className="select" onChange={handleDistrictChange}>
+            <option value="all">All</option>
             {districts?.map((district) => {
               return (
                 <option value={district} key={district}>
@@ -40,6 +56,8 @@ const StateCard = ({ state }) => {
         <p>Confirmed - {state?.total?.confirmed || "NA"}</p>
         <p>Recovered - {state?.total?.recovered || "NA"}</p>
         <p>Deceased - {state?.total?.deceased || "NA"}</p>
+        <p>1st dose done - {state?.total?.vaccinated1 || "NA"}</p>
+        <p>2 doses done - {state?.total?.vaccinated2 || "NA"}</p>
       </div>
     </article>
   );
--- src/pages/stateDetails.js
+++ src/pages/stateDetails.js
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useApi from "../api/useAPI";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSort, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const StateDetails = () => {
   const { state } = useParams();
-  const [info, setInfo] = useState(null);
+  const history = useNavigate();
+  const [info, setInfo] = useState({});
+  const [sortObject, setSortObject] = useState({
+    confirmed: "desc",
+    affected: "desc",
+    vaccinated: "desc",
+  });
   const { loading, data, error } = useApi(
     "https://data.covid19india.org/v4/min/timeseries.min.json"
   );
@@ -12,28 +20,160 @@ const StateDetails = () => {
   useEffect(() => {
     if (data) {
       let key,
-        dataCopy = JSON.parse(JSON.stringify(data)),
-        infoCopy = [];
+        dataCopy = JSON.parse(JSON.stringify(data));
       for (key in dataCopy) {
         if (key === state) {
-          dataCopy[key].state = key;
-          infoCopy.push(dataCopy[key]);
+          setInfo(dataCopy[key]["dates"]);
         }
       }
-      setInfo(infoCopy);
     }
-  }, [data]);
+  }, [data, state]);
+
+  const sortByCount = () => {
+    let infoCopy = JSON.parse(JSON.stringify(info));
+    if (sortObject.confirmed === "asc") {
+      infoCopy = infoCopy.sort((state1, state2) => {
+        if (state1.total.confirmed < state2.total.confirmed) {
+          return 1;
+        } else {
+          return -1;
+        }
+      });
+      setSortObject({
+        ...sortObject,
+        confirmed: "desc",
+      });
+    } else if (sortObject.confirmed === "desc") {
+      infoCopy = infoCopy.sort((state1, state2) => {
+        if (state1.total.confirmed < state2.total.confirmed) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+      setSortObject({
+        ...sortObject,
+        confirmed: "asc",
+      });
+    }
+    setInfo(infoCopy);
+  };
+
+  const sortByRecovered = () => {
+    console.log("clicked");
+  };
+
+  const sortByDeceased = () => {
+    console.log("clicked");
+  };
 
   if (loading) return <p>Loading</p>;
   if (error) return <p>Error</p>;
 
-  console.log(data);
+  const onDateChange = (e) => {
+    let key,
+      dataCopy = JSON.parse(JSON.stringify(data));
+    if (!e.target.value) {
+      for (key in dataCopy) {
+        if (key === state) {
+          setInfo(dataCopy[key]["dates"]);
+        }
+      }
+    } else {
+      let object = {};
+      object[e.target.value] = dataCopy[state]["dates"][e.target.value];
+      setInfo(object);
+    }
+  };
   return (
     <>
       <div className="header">
-        <h1>Covid Tracker - India</h1>
+        <h1>
+          Covid Tracker - India{" "}
+          <span
+            onClick={() => {
+              history(-1);
+            }}
+            style={{
+              cursor: "pointer",
+              float: "right",
+              fontSize: "1rem",
+              fontWeight: "normal",
+              color: "red",
+            }}
+          >
+            <FontAwesomeIcon icon={faArrowLeft} className="mr-2" />
+            Back
+          </span>
+        </h1>
       </div>
-      <div>state - {state}</div>
+      <div className="toolbar">
+        <div>
+          <input type="date" onChange={onDateChange} />
+        </div>
+        <div>
+          <span>
+            <button className="sortbtn" onClick={sortByCount}>
+              Sort by Confirmed count
+              <FontAwesomeIcon icon={faSort} className="ml-2" />
+            </button>
+          </span>
+          <span>
+            <button className="sortbtn" onClick={sortByRecovered}>
+              Sort by Recovered count
+              <FontAwesomeIcon icon={faSort} className="ml-2" />
+            </button>
+          </span>
+          <span>
+            <button className="sortbtn" onClick={sortByDeceased}>
+              Sort by Deceased count
+              <FontAwesomeIcon icon={faSort} className="ml-2" />
+            </button>
+          </span>
+        </div>
+      </div>
+      <div>
+        <table className="table">
+          <thead>
+            <th>Date</th>
+            <th>Confirmed</th>
+            <th>Recovered</th>
+            <th>Deceased</th>
+            <th>Delta</th>
+            <th>Delta7</th>
+          </thead>
+          <tbody>
+            {Object?.keys(info)?.map((key) => {
+              return (
+                <tr>
+                  <td>{key}</td>
+                  <td>{info[key]?.total?.confirmed || "NA"}</td>
+                  <td>{info[key]?.total?.recovered || "NA"}</td>
+                  <td>{info[key]?.total?.deceased || "NA"}</td>
+                  <td>
+                    <p>{`Confirmed - ${
+                      info[key]?.delta?.confirmed || "NA"
+                    }`}</p>
+                    <p>{`Recovered - ${
+                      info[key]?.delta?.recovered || "NA"
+                    }`}</p>
+                    <p>{`Deceased - ${info[key]?.delta?.deceased || "NA"}`}</p>
+                  </td>
+                  <td>
+                    <p>{`Confirmed - ${
+                      info[key]?.delta7?.confirmed || "NA"
+                    }`}</p>
+                    <p>{`Recovered - ${
+                      info[key]?.delta7?.recovered || "NA"
+                    }`}</p>
+                    <p>{`Deceased - ${info[key]?.delta7?.deceased || "NA"}`}</p>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
       <hr />
     </>
   );
